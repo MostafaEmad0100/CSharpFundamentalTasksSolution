36c51b74c91a472ee4f77794870ea2eb2a04857e
Mostafa Emad <Mostafa Emad@DESKTOP-92G1UR8>
2020-03-21 20:45:08 -0700

    sd

--- CSharpFundamentalTasksSolutions/Program.cs
+++ CSharpFundamentalTasksSolutions/Program.cs
@@ -11,6 +11,7 @@ namespace CSharpFundamentalTasksSolutions
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.ReadLine();
 
         }
     }
